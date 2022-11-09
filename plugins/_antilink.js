b9c0699b3aec7bbfb06bee330662b18fdda1a200
watama15 <109936711+watama15@users.noreply.github.com>
2022-11-09 21:31:01 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,23 +1,23 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-export async function before(m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isGroupLink = linkRegex.exec(m.text)
-    let hapus = m.key.participant
-    let bang = m.key.id
-    if (chat.antiLink && isGroupLink && !isAdmin) {
-        if (isBotAdmin) {
-            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) return !0
-        }
-        await conn.sendButton(m.chat, `*Group link detect!* ${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, author, ['off antilink', '/disable antilink'], m)
-        if (isBotAdmin && bot.restrict) {
-        return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-        return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*')
-    }
-    return !0
+let handler = m => m
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.db.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
+
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
+    if (isAdmin) return m.reply('*Eh sorry you admins, you will not be kicked. hehe..*')
+    if (!isBotAdmin) return m.reply('*Bots are not admins, how can they kick people _-*')
+    let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
+    let isLinkconnGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkconnGc.test(m.text)
+    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nOrder denied, bot will not kick you.\nBecause the group link itself')
+    await conn.sendMessage(m.chat, { delete: m.key })
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+  }
+  return true
 }
+
+module.exports = handler
